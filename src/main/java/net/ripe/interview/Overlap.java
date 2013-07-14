ec28806f7990daa8ee83026eb835d7bced2a7a0b
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-07-15 01:19:27 +0200

    add fields

--- src/main/java/net/ripe/interview/Overlap.java
+++ src/main/java/net/ripe/interview/Overlap.java
@@ -2,4 +2,13 @@ package net.ripe.interview;
 
 public class Overlap {
 
+	final Range first;
+	final Range second;
+	
+	public Overlap(Range first, Range second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	
 }
--- src/main/java/net/ripe/interview/Range.java
+++ src/main/java/net/ripe/interview/Range.java
@@ -2,4 +2,12 @@ package net.ripe.interview;
 
 public class Range {
 
+	final int left;
+	final int right;
+	
+	public Range(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+	
 }
