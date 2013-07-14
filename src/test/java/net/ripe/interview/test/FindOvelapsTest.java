eb6a589aae97a2a64bdd5efab8d0407179814589
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-07-15 01:35:32 +0200

    add failing test

--- src/test/java/net/ripe/interview/test/FindOvelapsTest.java
+++ src/test/java/net/ripe/interview/test/FindOvelapsTest.java
@@ -1,29 +1,43 @@
 package net.ripe.interview.test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import net.ripe.interview.Overlap;
+import net.ripe.interview.OverlapFinder;
+import net.ripe.interview.Range;
+
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class FindOvelapsTest {
 
-//	@Test
-//	public void doesNotOverlapWhenRangesTouchButDoNotIntersect() {
-//		
-//	}
-//	
-//	
-//	@Test
-//	public void findsSimpleOverlap() {
-//		
-//	}
-//	
-//	@Test 
-//	public void doesNotReturnDuplicateOverlaps() {
-//		
-//	}
-//	
-//	@Test
-//	public void returnsOverlapWhenOneRangeContainsAnother() {
-//		
-//	}
-	
-	
+	// @Test
+	// public void doesNotOverlapWhenRangesTouchButDoNotIntersect() {
+	//
+	// }
+
+	@Test
+	public void findsSimpleOverlap() {
+
+		Range first = new Range(1, 3);
+		Range second = new Range(2, 5);
+
+		Overlap expected = new Overlap(first, second);
+
+		List<Overlap> result = OverlapFinder.findOverlaps(Arrays.asList(first, second));
+		assertTrue(result.contains(expected));
+
+	}
+
+	// @Test
+	// public void doesNotReturnDuplicateOverlaps() {
+	//
+	// }
+	//
+	// @Test
+	// public void returnsOverlapWhenOneRangeContainsAnother() {
+	//
+	// }
+
 }
