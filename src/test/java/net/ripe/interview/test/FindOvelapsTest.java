3ffc320c2ea4faecba46fb3acfe005a582192917
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-07-15 01:25:43 +0200

    add test class with tests not yet implemented

---
+++ src/test/java/net/ripe/interview/test/FindOvelapsTest.java
@@ -0,0 +1,29 @@
+package net.ripe.interview.test;
+
+import org.junit.Test;
+
+public class FindOvelapsTest {
+
+//	@Test
+//	public void doesNotOverlapWhenRangesTouchButDoNotIntersect() {
+//		
+//	}
+//	
+//	
+//	@Test
+//	public void findsSimpleOverlap() {
+//		
+//	}
+//	
+//	@Test 
+//	public void doesNotReturnDuplicateOverlaps() {
+//		
+//	}
+//	
+//	@Test
+//	public void returnsOverlapWhenOneRangeContainsAnother() {
+//		
+//	}
+	
+	
+}
