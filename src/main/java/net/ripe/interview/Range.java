0b0d028128293c03332446743a7923687ef44133
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-07-15 02:35:07 +0200

    can now query ranges for intersections

--- src/main/java/net/ripe/interview/Range.java
+++ src/main/java/net/ripe/interview/Range.java
@@ -9,5 +9,21 @@ public class Range {
 		this.left = left;
 		this.right = right;
 	}
+
+	public boolean instersects(Range that) {
+		if(withinRange(that.left)) {
+			return this.right > that.left;
+		}
+		
+		if(withinRange(that.right)) {
+			return this.left < that.right;
+		}
+		
+		return this.left > that.left && this.right < that.right;
+	}
+
+	private boolean withinRange(int number) {
+		return number >= left && number <= right;
+	}
 	
 }
---
+++ src/test/java/net/ripe/interview/test/RangeIntersectionQueryTest.java
@@ -0,0 +1,68 @@
+package net.ripe.interview.test;
+
+import net.ripe.interview.Range;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RangeIntersectionQueryTest {
+
+	
+	@Test
+	public void whenEdgesTouchThereIsNoIntersection() {
+		
+		Range untilTwo = new Range(1,2);
+		Range afterTwo = new Range(2,3);
+		
+		assertDoesNotIntersect(untilTwo, afterTwo);
+		
+		
+	}
+
+
+	private void assertDoesNotIntersect(Range untilTwo, Range afterTwo) {
+		assertFalse(untilTwo.instersects(afterTwo));
+		assertFalse(afterTwo.instersects(untilTwo));
+	}
+	
+	
+	@Test
+	public void whenOneRangeIsContainedInAnotherThereIsAnIntersection() {
+		
+		Range oneToFive = new Range(1, 5);
+		Range twoToThree = new Range(2,3);
+		
+		assertIntersects(oneToFive, twoToThree);
+		
+	}
+	
+	
+	private void assertIntersects(Range oneToFive, Range twoToThree) {
+		assertTrue(oneToFive.instersects(twoToThree));
+		assertTrue(twoToThree.instersects(oneToFive));
+	}
+
+
+	@Test
+	public void confirmsSimpleIntersectionWhenItExists() {
+		
+		Range oneToThree = new Range(1,3);
+		Range twoToFour = new Range(2,4);
+		
+		assertIntersects(oneToThree, twoToFour);
+		
+		
+		
+	}
+	
+	@Test
+	public void noIntersectionObservedWhenThereIsNone() {
+		
+		Range oneToTwo = new Range(1,2);
+		Range fourToFive = new Range(4,5);
+		
+		assertDoesNotIntersect(oneToTwo, fourToFive);
+		
+	}
+	
+}
