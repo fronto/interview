ce740e692a9d2ca3834bb073038c40cd02f49f9d
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-07-15 03:44:52 +0200

    added equals and hashcode where possible, private fields

--- src/main/java/net/ripe/interview/Overlap.java
+++ src/main/java/net/ripe/interview/Overlap.java
@@ -2,13 +2,36 @@ package net.ripe.interview;
 
 public class Overlap {
 
-	final Range first;
-	final Range second;
-	
+	private final Range first;
+	private final Range second;
+
 	public Overlap(Range first, Range second) {
 		this.first = first;
 		this.second = second;
 	}
-	
-	
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (obj == this) {
+			return true;
+		}
+
+		if (!(obj instanceof Overlap)) {
+			return false;
+		}
+
+		Overlap overlap = (Overlap) obj;
+
+		return overlap.hasRange(first) && overlap.hasRange(second);
+
+	}
+
+	// NB Skipped hashcode deliberately, as ordering of Range elements should not matter,
+	// but could unwittingly affect hashcode if implemented
+
+	private boolean hasRange(Range range) {
+		return first.equals(range) || second.equals(range);
+	}
+
 }
--- src/main/java/net/ripe/interview/Range.java
+++ src/main/java/net/ripe/interview/Range.java
@@ -2,10 +2,13 @@ package net.ripe.interview;
 
 public class Range {
 
-	final int left;
-	final int right;
+	private final int left;
+	private final int right;
 	
 	public Range(int left, int right) {
+		if(left >= right) {
+			throw new IllegalArgumentException("left must be less than right");
+		}
 		this.left = left;
 		this.right = right;
 	}
@@ -24,6 +27,30 @@ public class Range {
 
 	private boolean withinRange(int number) {
 		return number >= left && number <= right;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == this) {
+			return true;
+		}
+		
+		if(!(obj instanceof Range)) {
+			return false;
+		}
+		
+		Range range = (Range) obj;
+		
+		return this.left == range.left && this.right == range.right;
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = 17;
+		result = 37*result + left;
+		result = 37*result + right;
+		return result;
 	}
 	
 }
