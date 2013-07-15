1e2fd0fc46e492d243a45e2b9abaff8eba312acc
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-07-15 04:22:24 +0200

    Implemented recrusive algorithm to avoid duplicates and added tests

--- src/main/java/net/ripe/interview/OverlapFinder.java
+++ src/main/java/net/ripe/interview/OverlapFinder.java
@@ -1,12 +1,37 @@
 package net.ripe.interview;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OverlapFinder {
 
 	public static List<Overlap> findOverlaps(List<Range> ranges) {
-		return null;
+
+		List<Overlap> overlaps = new ArrayList<Overlap>();
+		return findOverlapsRecursively(overlaps, ranges);
+
 	}
-	
-	
+
+	private static List<Overlap> findOverlapsRecursively(List<Overlap> overlapsSoFar, List<Range> remainingRanges) {
+
+		if (remainingRanges.size() < 2) {
+			return overlapsSoFar;
+		}
+
+		Range first = remainingRanges.get(0);
+
+		for (int index = 1; index < remainingRanges.size(); index++) {
+
+			Range next = remainingRanges.get(index);
+			if (first.instersects(next)) {
+				Overlap overlap = new Overlap(first, next);
+				overlapsSoFar.add(overlap);
+			}
+
+		}
+
+		remainingRanges = remainingRanges.subList(1, remainingRanges.size());
+
+		return findOverlapsRecursively(overlapsSoFar, remainingRanges);
+	}
 }
--- src/test/java/net/ripe/interview/test/FindOvelapsTest.java
+++ src/test/java/net/ripe/interview/test/FindOvelapsTest.java
@@ -1,5 +1,9 @@
 package net.ripe.interview.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,15 +11,40 @@ import net.ripe.interview.Overlap;
 import net.ripe.interview.OverlapFinder;
 import net.ripe.interview.Range;
 
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.hamcrest.Matcher;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class FindOvelapsTest {
 
-	// @Test
-	// public void doesNotOverlapWhenRangesTouchButDoNotIntersect() {
-	//
-	// }
+	private static final int ONE = 1;
+
+	@Test
+	public void doesNotOverlapWhenRangesTouchButDoNotIntersect() {
+
+		Range oneToTwo = new Range(1, 2);
+		Range twoToFour = new Range(2, 4);
+
+		assertThat(overlapsFrom(oneToTwo, twoToFour), isEmpty());
+
+	}
+
+	private Matcher<List<Overlap>> isEmpty() {
+		return new CustomTypeSafeMatcher<List<Overlap>>("should be empty") {
+
+			@Override
+			protected boolean matchesSafely(List<Overlap> item) {
+				return item.isEmpty();
+			}
+		};
+
+	}
+
+	private List<Overlap> overlapsFrom(Range... ranges) {
+
+		return OverlapFinder.findOverlaps(Arrays.asList(ranges));
+
+	}
 
 	@Test
 	public void findsSimpleOverlap() {
@@ -25,19 +54,41 @@ public class FindOvelapsTest {
 
 		Overlap expected = new Overlap(first, second);
 
-		List<Overlap> result = OverlapFinder.findOverlaps(Arrays.asList(first, second));
-		assertTrue(result.contains(expected));
+		assertTrue(overlapsFrom(first, second).contains(expected));
 
 	}
 
-	// @Test
-	// public void doesNotReturnDuplicateOverlaps() {
-	//
-	// }
-	//
-	// @Test
-	// public void returnsOverlapWhenOneRangeContainsAnother() {
-	//
-	// }
+	@Test
+	public void doesNotOverlapWhenRangesDoNotIntersect() {
+
+		Range first = new Range(1, 2);
+		Range second = new Range(3, 5);
+
+		assertThat(overlapsFrom(first, second), isEmpty());
+
+	}
+
+	@Test
+	public void doesNotReturnDuplicateOverlaps() {
+		Range first = new Range(1, 3);
+		Range second = new Range(2, 5);
+		Range doesNotOverlap = new Range(7, 9);
+
+		List<Overlap> result = overlapsFrom(first, second, doesNotOverlap);
+		assertEquals(ONE, result.size());
+
+	}
+
+	@Test
+	public void returnsOverlapWhenOneRangeContainsAnother() {
+
+		Range inner = new Range(2, 3);
+		Range outer = new Range(1, 4);
+
+		Overlap expected = new Overlap(inner, outer);
+
+		assertTrue(overlapsFrom(inner, outer).contains(expected));
+
+	}
 
 }
