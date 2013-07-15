93900d13fe45b0573678b7c94c4aca34a458a831
Thomas FItzpatrick <thomasfitzpatrick@thomasf.local>
2013-07-15 02:39:43 +0200

    format code

--- src/test/java/net/ripe/interview/test/RangeIntersectionQueryTest.java
+++ src/test/java/net/ripe/interview/test/RangeIntersectionQueryTest.java
@@ -7,62 +7,54 @@ import static org.junit.Assert.*;
 
 public class RangeIntersectionQueryTest {
 
-	
 	@Test
 	public void whenEdgesTouchThereIsNoIntersection() {
-		
-		Range untilTwo = new Range(1,2);
-		Range afterTwo = new Range(2,3);
-		
-		assertDoesNotIntersect(untilTwo, afterTwo);
-		
-		
-	}
 
+		Range untilTwo = new Range(1, 2);
+		Range afterTwo = new Range(2, 3);
+
+		assertDoesNotIntersect(untilTwo, afterTwo);
+
+	}
 
 	private void assertDoesNotIntersect(Range untilTwo, Range afterTwo) {
 		assertFalse(untilTwo.instersects(afterTwo));
 		assertFalse(afterTwo.instersects(untilTwo));
 	}
-	
-	
+
 	@Test
 	public void whenOneRangeIsContainedInAnotherThereIsAnIntersection() {
-		
+
 		Range oneToFive = new Range(1, 5);
-		Range twoToThree = new Range(2,3);
-		
+		Range twoToThree = new Range(2, 3);
+
 		assertIntersects(oneToFive, twoToThree);
-		
+
 	}
-	
-	
+
 	private void assertIntersects(Range oneToFive, Range twoToThree) {
 		assertTrue(oneToFive.instersects(twoToThree));
 		assertTrue(twoToThree.instersects(oneToFive));
 	}
 
-
 	@Test
 	public void confirmsSimpleIntersectionWhenItExists() {
-		
-		Range oneToThree = new Range(1,3);
-		Range twoToFour = new Range(2,4);
-		
+
+		Range oneToThree = new Range(1, 3);
+		Range twoToFour = new Range(2, 4);
+
 		assertIntersects(oneToThree, twoToFour);
-		
-		
-		
+
 	}
-	
+
 	@Test
 	public void noIntersectionObservedWhenThereIsNone() {
-		
-		Range oneToTwo = new Range(1,2);
-		Range fourToFive = new Range(4,5);
-		
+
+		Range oneToTwo = new Range(1, 2);
+		Range fourToFive = new Range(4, 5);
+
 		assertDoesNotIntersect(oneToTwo, fourToFive);
-		
+
 	}
-	
+
 }
